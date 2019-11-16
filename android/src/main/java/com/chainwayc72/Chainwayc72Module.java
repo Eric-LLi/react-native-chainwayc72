2b07c8d2ca325959a5ea63b0e7248600a003bc69
Eric-LLi <ericli1116@gmail.com>
2019-11-17 02:22:00 +1100

    update

--- android/src/main/java/com/chainwayc72/Chainwayc72Module.java
+++ android/src/main/java/com/chainwayc72/Chainwayc72Module.java
@@ -8,6 +8,7 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.WritableArray;
 import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
 
 public class Chainwayc72Module extends ReactContextBaseJavaModule implements LifecycleEventListener {
